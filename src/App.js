afb295dbf07bf05f10bdcc2ca42fba8048a8db56
Rohit Dhatrak <rdhatrak19@gmail.com>
2020-12-09 13:02:48 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+let food = {
+  "🍇": "Grapes",
+  "🍈": "Melon",
+  "🍉": "Watermelon",
+  "🍊": "Tangerine",
+  "🍋": "Lemon",
+  "🍌": "Banana",
+  "🍍": "Pineapple",
+  "🥭": "Mango",
+  "🍎": "Red Apple",
+  "🍏": "Green Apple",
+  "🍐": "Pear",
+  "🍑": "Peach",
+  "🍒": "Cherries",
+  "🍓": "Strawberry",
+  "🥝": "Kiwi Fruit",
+  "🍅": "Tomato",
+  "🥥": "Coconut",
+  "🥑": "Avocado",
+  "🍆": "Eggplant",
+  "🥔": "Potato",
+  "🥕": "Carrot",
+  "🌽": "Ear of Corn",
+  "🌶️": "Hot Pepper",
+  "🥒": "Cucumber",
+  "🥬": "Leafy Green",
+  "🥦": "Broccoli",
+  "🧄": "Garlic",
+  "🧅": "Onion",
+  "🍄": "Mushroom",
+  "🥜": "Peanuts",
+  "🌰": "Chestnut",
+  "🍞": "Bread",
+  "🥐": "Croissant",
+  "🥖": "Baguette Bread",
+  "🥨": "Pretzel",
+  "🥯": "Bagel",
+  "🥞": "Pancakes",
+  "🧇": "Waffle",
+  "🧀": "Cheese Wedge",
+  "🍗": "Poultry Leg",
+  "🥩": "Cut of Meat",
+  "🥓": "Bacon",
+  "🍔": "Hamburger",
+  "🍟": "French Fries",
+  "🍕": "Pizza",
+  "🌭": "Hot Dog",
+  "🥪": "Sandwich",
+  "🌮": "Taco",
+  "🌯": "Burrito",
+  "🥙": "Stuffed Flatbread",
+  "🥚": "Egg",
+  "🥗": "Green Salad",
+  "🍿": "Popcorn",
+  "🍘": "Rice Cracker",
+  "🍙": "Rice Ball",
+  "🍚": "Cooked Rice",
+  "🍛": "Curry Rice",
+  "🍝": "Spaghetti",
+  "🍠": "Roasted Sweet Potato",
+  "🍣": "Sushi",
+  "🍤": "Fried Shrimp",
+  "🍥": "Fish Cake with Swirl",
+  "🥮": "Moon Cake",
+  "🍡": "Dango",
+  "🥟": "Dumpling",
+  "🥠": "Fortune Cookie",
+  "🦪": "Oyster",
+  "🍦": "Soft Ice Cream",
+  "🍧": "Shaved Ice",
+  "🍨": "Ice Cream",
+  "🍩": "Doughnut",
+  "🍪": "Cookie",
+  "🎂": "Birthday Cake",
+  "🍰": "Shortcake",
+  "🧁": "Cupcake",
+  "🥧": "Pie",
+  "🍫": "Chocolate Bar",
+  "🍬": "Candy",
+  "🍭": "Lollipop",
+  "🍮": "Custard"
+};
+
+let foodArray = Object.keys(food);
+
+export default function App() {
+  let [emoji, updateEmoji] = useState("");
+  function handleInput(inp) {
+    updateEmoji(food[inp.target.value]);
+  }
+
+  function emojiHandler(emoji) {
+    updateEmoji(food[emoji]);
+  }
+
+  return (
+    <div className="App">
+      <h1 className="header">Food Emoji's</h1>
+      <h2 className="ans">{emoji}</h2>
+      <input
+        className="input"
+        onChange={handleInput}
+        placeholder="Enter your emoji here"
+      ></input>
+      <h3 className="text">Or select one from below</h3>
+      <div className="container">
+        {foodArray.map((key) => (
+          <span className="items" onClick={() => emojiHandler(key)}>
+            {key}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+}
